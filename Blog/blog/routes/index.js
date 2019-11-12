c544a1c1bc5a109717e1dc1dc11ca7cd06f55150
Fengfeike <2088399108@qq.com>
2019-11-12 19:15:16 +0800

    update
    update

---
+++ Blog/blog/routes/index.js
@@ -0,0 +1,22 @@
+var express = require('express');
+var {users,chapterList} = require('../data.json');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+router.post('/',function(req,res,next){
+  if(users[0].username === req.body.username && users[0].password === req.body.password){
+    res.redirect('/list');
+  }else if(req.body.username === '' && req.body.password !== ''){
+    res.end('username or password is wrong!')
+  }else if(req.body.password === '' && req.body.username !== ''){
+    res.end('username or password is wrong!')
+  }else if(req.body.username === '' && req.body.password === ''){
+    res.end('username or password is wrong!')
+  }else{
+    res.end('username or password is wrong!')
+  }
+});
+module.exports = router;
